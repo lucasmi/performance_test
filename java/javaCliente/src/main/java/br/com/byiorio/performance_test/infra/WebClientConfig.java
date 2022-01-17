404872ce7de0e0792a44619219d8cc57300127c2
Lucas Iorio <lucas.iorio@gmail.com>
2022-01-17 17:20:08 -0300

    limpando import

--- java/javaCliente/src/main/java/br/com/byiorio/performance_test/infra/WebClientConfig.java
+++ java/javaCliente/src/main/java/br/com/byiorio/performance_test/infra/WebClientConfig.java
@@ -1,7 +1,5 @@
 package br.com.byiorio.performance_test.infra;
 
-import java.time.Duration;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
